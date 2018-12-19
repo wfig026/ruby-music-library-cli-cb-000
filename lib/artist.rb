905193f015c9e98b30addc3e77f7abcff4b9b437
Will Figueiredo <will@wfigueiredo.com>
2018-12-19 19:32:54 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,38 @@
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+  def add_song(song)
+    song.artist ? nil : song.artist = self
+    @songs.include?(song) ? nil : @songs << song
+  end
+
+  def genres
+    self.songs.collect{ |song| song.genre }.uniq
+  end
+end
